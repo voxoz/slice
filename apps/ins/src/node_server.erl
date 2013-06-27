782eb690e947d98f188a168250472710825e1881
Maxim Sokhatsky <maxim@synrc.com>
2013-06-27 04:32:18 +0300

    LXC creation schema

--- apps/ins/src/node_server.erl
+++ apps/ins/src/node_server.erl
@@ -16,6 +16,30 @@ create(User,Token,Cpu,Ram,Cert,Ports) ->
          ok -> create_box(User,Cpu,Ram,Cert,Ports);
          Error -> Error end.
 
+% LXC creation schema
+
+% ports: 22, 80, 2000
+% user: maxim
+% cpu: 8
+% ram: 128000000
+
+% makepasswd --char=12
+% > L96QBmh21gKb
+% docker build .
+% >  ---> Running in e2f2668a6ab5
+% >  ---> 79f6c9f416d2
+% > Successfully built 79f6c9f416d2
+% docker commit e2f2668a6ab5 synrc/sncn1
+% docker push synrc/sncn1
+% docker run -d -p 22 -p 80 -c=8 -m=128000000 synrc/sncn1 /usr/bin/supervisor -n
+% > b33e7a0a354c
+% docker port b33e7a0a354c 22
+% > 49158
+% docker port b33e7a0a354c 80
+% > 49159
+
+% mail: IP=do1.synrc.com, ROOT_PASS=L96QBmh21gKb, NAME=sncn1, ID=387ba01740ed, SSH_PORT=49158
+
 create_box(User,Cpu,Ram,Cert,Ports) ->
     P = string:join([ "-p " ++ integer_to_list(Port) || Port <- Ports], " "),
     Hostname = "sncn" ++ integer_to_list(kvs:next_id(feed)),
